d89a0a087d79aeb49456ad7c7e8c0d68cc398991
Pooja Chaudhri <pschaudhri2003@gmail.com>
2025-09-23 15:44:43 +0530

    Update app-constant.ts

--- ashokit-e-comm-application/src/app/app-constant.ts
+++ ashokit-e-comm-application/src/app/app-constant.ts
@@ -1,25 +1,15 @@
 export class AppConstant {
 
-    static prodcuct_endpoint = "http://localhost:8081/AllProduct";
+    static prodcuct_endpoint = "http://product-api:8081/AllProduct";
+    static category_endpoint = "http://product-api:8081/product";
+    static Allcategory_endpoint = "http://product-api:8081/category";
+    static search_endpoint = "http://product-api:8081/productByName";
 
-    static category_endpoint = "http://localhost:8081/product";
+    static order_endpoint = "http://orders-api:8081/createOrder";
+    static Putorder_endpoint = "http://orders-api:8081/updateOrder";
 
-    static Allcategory_endpoint = "http://localhost:8081/category";
-
-    static search_endpoint = "http://localhost:8081/productByName";
-
-    static order_endpoint = "http://localhost:9090/createOrder";
-
-    static Putorder_endpoint = "http://localhost:9090/updateOrder";
-
-    static login_endpoint = "http://localhost:8082/login";
-
-    static register_endpoint = "http://localhost:8082/register";
-
-    static reset_endpoint = "http://localhost:8082/resetPwd";
-
-    static forgot_endpoint = "http://localhost:8082/forgotpwd"
-
-
-    
+    static login_endpoint = "http://customer-api:8081/login";
+    static register_endpoint = "http://customer-api:8081/register";
+    static reset_endpoint = "http://customer-api:8081/resetPwd";
+    static forgot_endpoint = "http://customer-api:8081/forgotpwd";
 }
