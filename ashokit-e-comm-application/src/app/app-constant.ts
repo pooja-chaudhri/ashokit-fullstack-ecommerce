4babdbcd999d3b68e2fc6e8b1a3e6def64c0aec2
pooja <pschaudhri2003@gmail.com>
2025-09-25 11:48:50 +0530

    add

--- ashokit-e-comm-application/src/app/app-constant.ts
+++ ashokit-e-comm-application/src/app/app-constant.ts
@@ -1,15 +1,16 @@
 export class AppConstant {
-
-    static prodcuct_endpoint = "http://product-api:8081/AllProduct";
-    static category_endpoint = "http://product-api:8081/product";
-    static Allcategory_endpoint = "http://product-api:8081/category";
-    static search_endpoint = "http://product-api:8081/productByName";
-
-    static order_endpoint = "http://orders-api:8081/createOrder";
-    static Putorder_endpoint = "http://orders-api:8081/updateOrder";
+    static prodcuct_endpoint    = "http://localhost:8083/AllProduct";
+    static category_endpoint    = "http://localhost:8083/product";
+    static Allcategory_endpoint = "http://localhost:8083/category";
+    static search_endpoint      = "http://localhost:8083/productByName";
 
     static login_endpoint = "http://customer-api:8081/login";
     static register_endpoint = "http://customer-api:8081/register";
     static reset_endpoint = "http://customer-api:8081/resetPwd";
     static forgot_endpoint = "http://customer-api:8081/forgotpwd";
+
+     // Orders API
+    static order_endpoint      = "http://orders-api:8082/createOrder";
+    static Putorder_endpoint   = "http://orders-api:8082/updateOrder";
+
 }
