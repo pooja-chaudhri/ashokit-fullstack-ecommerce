aac1a1044dbc2aad86e51d08aca660cce1b222c4
Pooja Chaudhri <pschaudhri2003@gmail.com>
2025-09-26 16:18:03 +0530

    Update app-constant.ts

--- ashokit-e-comm-application/src/app/app-constant.ts
+++ ashokit-e-comm-application/src/app/app-constant.ts
@@ -1,17 +1,17 @@
 export class AppConstant {
-    // Product API
-    static product_endpoint    = "http://product-api:8083/AllProduct";
-    static category_endpoint   = "http://product-api:8083/product";
-    static Allcategory_endpoint= "http://product-api:8083/category";
-    static search_endpoint     = "http://product-api:8083/productByName";
+  // Product API
+  static product_endpoint    = "http://43.204.220.254:8083/AllProduct";
+  static category_endpoint   = "http://43.204.220.254:8083/product";
+  static Allcategory_endpoint= "http://43.204.220.254:8083/category";
+  static search_endpoint     = "http://43.204.220.254:8083/productByName";
 
-    // Customer API
-    static login_endpoint      = "http://customer-api:8081/login"; 
-    static register_endpoint   = "http://customer-api:8081/register"; 
-    static reset_endpoint      = "http://customer-api:8081/resetPwd"; 
-    static forgot_endpoint     = "http://customer-api:8081/forgotpwd"; 
+  // Customer API
+  static login_endpoint      = "http://43.204.220.254:8081/login"; 
+  static register_endpoint   = "http://43.204.220.254:8081/register"; 
+  static reset_endpoint      = "http://43.204.220.254:8081/resetPwd"; 
+  static forgot_endpoint     = "http://43.204.220.254:8081/forgotpwd"; 
 
-    // Orders API
-    static order_endpoint      = "http://orders-api:8082/createOrder"; 
-    static Putorder_endpoint   = "http://orders-api:8082/updateOrder"; 
+  // Orders API
+  static order_endpoint      = "http://43.204.220.254:8082/createOrder"; 
+  static Putorder_endpoint   = "http://43.204.220.254:8082/updateOrder"; 
 }
